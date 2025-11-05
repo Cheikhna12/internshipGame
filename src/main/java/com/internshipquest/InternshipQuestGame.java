8da797bbdbe6b3563ac13999e2e8a0603d607724
Plantevin <thomas.plantevin@epitech.eu>
2025-11-05 14:37:22 +0100

    feat(lieuvisitable): add action in logic

---
+++ src/main/java/com/internshipquest/InternshipQuestGame.java
@@ -0,0 +1,30 @@
+package com.internshipquest;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.internshipquest.screens.WorldMapScreen;
+
+public class InternshipQuestGame extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font; // <- ajouté
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont(); // <- police par défaut
+        setScreen(new WorldMapScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        if (batch != null) batch.dispose();
+        if (font != null) font.dispose(); // <- on dispose de la font
+        if (getScreen() != null) getScreen().dispose();
+    }
+}
