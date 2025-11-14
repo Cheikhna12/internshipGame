d17cf4fa44d4111e8b04111dfbfa045a46de55c1
Plantevin <thomas.plantevin@epitech.eu>
2025-11-14 15:05:12 +0100

    feat(NPC) add NPC image

--- src/main/java/com/internshipquest/model/location/Epitech.java
+++ src/main/java/com/internshipquest/model/location/Epitech.java
@@ -4,11 +4,13 @@ import com.internshipquest.InternshipQuestGame;
 import com.internshipquest.model.activity.AActivity;
 import com.internshipquest.model.activity.ActivityFactory;
 import com.internshipquest.utils.SoundManager;
+import com.badlogic.gdx.graphics.Texture;
 
 import java.util.List;
 
 public class Epitech extends ALieuVisitable {
 
+    private Texture profTexture;
 
     public Epitech(InternshipQuestGame game) {
         super(game);
@@ -16,6 +18,7 @@ public class Epitech extends ALieuVisitable {
         this.closedHour = 19;
         this.openOnWeekends = false;
         activities = ActivityFactory.getEpitechActivities(this);
+        profTexture = new Texture("assets/prof.png");
     }
 
     @Override
@@ -27,6 +30,22 @@ public class Epitech extends ALieuVisitable {
     @Override
     public void onExit() {
         SoundManager.stopMusic();
+    }
+
+    @Override
+    public Texture getNpcTexture() {
+        return profTexture;
+    }
+
+    @Override
+    public String getNpcMessage() {
+        return "There you are!,\n Did you remember to swipe your badge?";
+    }
+
+
+    public void dispose() {
+        if (profTexture != null)
+            profTexture.dispose();
     }
 }
 
--- src/main/java/com/internshipquest/model/location/Shop.java
+++ src/main/java/com/internshipquest/model/location/Shop.java
@@ -4,11 +4,13 @@ import com.internshipquest.InternshipQuestGame;
 import com.internshipquest.model.activity.AActivity;
 import com.internshipquest.model.activity.ActivityFactory;
 import com.internshipquest.utils.SoundManager;
+import com.badlogic.gdx.graphics.Texture;
 
 import java.util.List;
 
 public class Shop extends ALieuVisitable {
 
+    private Texture ShopOwnerTexture;
 
     public Shop(InternshipQuestGame game) {
         super(game);
@@ -16,6 +18,7 @@ public class Shop extends ALieuVisitable {
         this.closedHour =20;
        this.openOnWeekends = true;
         activities = ActivityFactory.getShopActivities(this);
+        ShopOwnerTexture = new Texture("assets/StoreOwner.png");
     }
 
     @Override
@@ -27,6 +30,22 @@ public class Shop extends ALieuVisitable {
     @Override
     public void onExit() {
         SoundManager.stopMusic();
+    }
+
+    @Override
+    public Texture getNpcTexture() {
+        return ShopOwnerTexture;
+    }
+
+    @Override
+    public String getNpcMessage() {
+        return "Sir, are you here for work\n or to buy something?";
+    }
+
+
+    public void dispose() {
+        if (ShopOwnerTexture != null)
+            ShopOwnerTexture.dispose();
     }
 }
 
